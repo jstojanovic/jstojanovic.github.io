09e3acdce71a884d7822c0ce1d075de2ba5660a7
jstojanovic <jstojanovic@student.uniri.hr>
2024-02-12 13:50:16 +0100

    12.Veljaca Naslovna clnp

---
+++ spaisekarte.js
@@ -0,0 +1,8 @@
+// Makes element visible via ID  
+btn_predjela = document.getElementById('predjelo');
+predjela_sva = document.getElementById('predjela_sva')
+
+
+btn_predjela.addEventListener('click', function() {
+    predjela_sva.style.display = 'block'
+})
